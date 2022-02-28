779fd265b24ed8717f37b232b83719bb4425cf27
pradeep <pradeep.misra@gmail.com>
2022-02-28 15:42:36 +0530

    Added tailwind config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,22 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+      <head>
+        <meta charset="UTF-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link href="/dist/main.css" rel="stylesheet"/>
+      </head>
+            <body>
+              <h1 class="text-3xl font-bold underline">
+                Hello world!
+              </h1>
+            </body>
+          </div>
+          );
 }
 
-export default App
+          export default App;
